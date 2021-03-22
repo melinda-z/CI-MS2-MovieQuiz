79e635341c0ff25c53a08dac1042bcfb2ee26f61
Melinda Zhang <melindazhang@Melindas-MacBook-Air.local>
2021-03-22 12:00:31 +0000

    got the showQuestion function working in game.js

--- assets/script/game.js
+++ assets/script/game.js
@@ -1,4 +1,56 @@
-const question = document.querySelector('question');
-const question = document.querySelector('question');
-const question = document.querySelector('question');
-const question = document.querySelector('question');
+questions = [
+  {
+    text: "Which planet skywalker is from1 ?",
+    answer: "Tatooine1",
+    choice2: "Mars1",
+    choice3: "Saturn1",
+    choice4: "Hoth1",
+  },
+  {
+    text: "Which planet skywalker is from2?",
+    answer: "Tatooine2",
+    choice2: "Mars2",
+    choice3: "Saturn2",
+    choice4: "Hoth2",
+  },
+  {
+    text: "Which planet skywalker is from3 ?",
+    answer: "Tatooine3",
+    choice2: "Mars3",
+    choice3: "Saturn3",
+    choice4: "Hoth3",
+  },
+  {
+    text: "Which planet skywalker is from4 ?",
+    answer: "Tatooine4",
+    choice2: "Mars4",
+    choice3: "Satur4",
+    choice4: "Hoth4",
+  },
+  {
+    text: "Which planet skywalker is from5 ?",
+    answer: "Tatooine5",
+    choice2: "Mars5",
+    choice3: "Saturn5",
+    choice4: "Hoth5",
+  },
+];
+currentQuestion = {};
+currentScore = 0;
+
+function getRandomIndex() {
+  min = Math.ceil(0);
+  max = Math.floor(questions.length - 1);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function showQuestion() {
+  question = questions[getRandomIndex()];
+  document.getElementById("question").innerHTML = question["text"];
+  document.getElementById("answer").innerHTML = question["answer"];
+  document.getElementById("choice2").innerHTML = question["choice2"];
+  document.getElementById("choice3").innerHTML = question["choice3"];
+  document.getElementById("choice4").innerHTML = question["choice4"];
+  currentQuestion = question;
+}
+showQuestion();
