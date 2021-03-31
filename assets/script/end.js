df0da4c8c27cb67b4ac3844ab5c91ca7e8f597c6
Melinda Zhang <melindazhang@Melindas-MacBook-Air.local>
2021-03-31 15:59:20 +0100

    sort and save top highest score in local storage

--- assets/script/end.js
+++ assets/script/end.js
@@ -4,6 +4,11 @@ const saveHighScoreButton = document.getElementById("save-score-btn");
 const finalScore = document.getElementById("final-score");
 // get the most recent score from local storage
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+// get the highscore that saved in the local storage and convert it into an array with JSON.parse
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+// set the max number of high scores going to be saved and displayed
+const MAX_HIGH_SCORES = 5;
 // display the most recent score on the end screen
 finalScore.innerText = mostRecentScore;
 
@@ -15,6 +20,21 @@ allowUserSave = () => {
 
 saveHighScore = (event) => {
   event.preventDefault();
+  // set the score to an object format
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  // push score into the high scores array
+  highScores.push(score);
+  // sort high scores
+  highScores.sort((a, b) => b.score - a.score);
+  // cut off the array at index 5 (show top 5 scores)
+  highScores.splice(5);
+  // set the high score to a string in local storage
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  // return to the home when score has been saved
+  window.location.assign("/");
 };
 
 // EVENTS
