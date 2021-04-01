3331a46e056032a673611ad1805ff139554e928e
Melinda Zhang <melindazhang@Melindas-MacBook-Air.local>
2021-04-01 15:19:37 +0100

    edited highscores html and css file

---
+++ assets/script/highScores.js
@@ -0,0 +1,12 @@
+// CONSTANTS
+const highScoresList = document.getElementById("highScoresList");
+// get the array of high scores stored in the local storage
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+highScoresList.innerHTML = highScores
+  // display each high score from the array using map method
+  .map((score) => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
+console.log(highScoresList);
