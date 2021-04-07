5601ed958675632bdf06409ccafc1bfe1a26db21
Melinda Zhang <melindazhang@Melindas-MacBook-Air.local>
2021-04-07 14:00:22 +0100

    added background music to home page

--- assets/script/home.js
+++ assets/script/home.js
@@ -5,6 +5,9 @@ const modal = document.getElementById("rules-modal");
 const btn = document.getElementById("rules-button");
 const closeModal = document.getElementById("close-modal");
 const music = document.getElementById("music");
+const musicOnButton = document.getElementById("musicOn");
+const musicOffButton = document.getElementById("musicOff");
+const backgroundMusic = document.getElementById("background-music");
 
 // FUNCTIONS
 // open the modal when the user clicks on the button
@@ -22,13 +25,20 @@ anyWhereClose = (event) => {
   }
 };
 // turn the music on, display the on icon and hide the mute icon
-musicOn = () => {
-  music.innerHTML = `<i class="fas fa-volume-up"></i>`;
-  music.classList.add("hidden");
+musicOnOff = () => {
+  musicOnButton.classList.toggle("hidden");
+  musicOffButton.classList.toggle("hidden");
+  if (musicOnButton.classList.contains("hidden")) {
+    backgroundMusic.play();
+    // put music on repeat
+    backgroundMusic.loop = true;
+  } else {
+    backgroundMusic.pause();
+  }
 };
 
 // EVENTS
 btn.addEventListener("click", openModal);
 closeModal.addEventListener("click", close);
 window.addEventListener("click", anyWhereClose);
-music.addEventListener("click", musicOn);
+music.addEventListener("click", musicOnOff);
