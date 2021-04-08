902ed243e1cc19daef83a3df291b15a48d8d20e4
Melinda Zhang <melindazhang@Melindas-MacBook-Air.local>
2021-04-08 17:22:55 +0100

    fix bug

--- assets/script/end.js
+++ assets/script/end.js
@@ -34,9 +34,7 @@ saveHighScore = (event) => {
   // set the high score to a string in local storage
   localStorage.setItem("highScores", JSON.stringify(highScores));
   // return to the home when score has been saved
-  window.location.assign(
-    "https://melindazhang2020.github.io/CI-MS2-MovieQuiz/index.html"
-  );
+  window.location.assign("index.html");
 };
 
 // EVENTS
